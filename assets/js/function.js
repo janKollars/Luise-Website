f41a2e96a7a86ab09016588258fc6974a748fd58
janKollars <kollars.jan@gmail.com>
2018-07-12 14:57:03 +0200

    updated dependencies + fixed line-height of figoverlay

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,21 +1,20 @@
 // Menu
-const burger = document.getElementById("burger");
-const nav = document.getElementById("top-nav");
+const burger = document.getElementById('burger')
+const nav = document.getElementById('top-nav')
 
-burger.addEventListener("click", function () {
-  burger.classList.toggle("is-open");
-  nav.classList.toggle("is-open");
-});
+burger.addEventListener('click', function () {
+  burger.classList.toggle('is-open')
+  nav.classList.toggle('is-open')
+})
 
 // Parallax https://daverupert.com/2018/02/cheapass-parallax/
-const title = document.getElementsByClassName('parallax-title')[0];
-console.log(title);
-const speed = 0.4;
-title.style.transform = 'translateY( calc( var(--scrollparallax) * 1px ) )';
+const title = document.getElementsByClassName('parallax-title')[0]
+const speed = 0.4
+title.style.transform = 'translateY( calc( var(--scrollparallax) * 1px ) )'
 
-function setScrollParallax() {
-    title.style.setProperty("--scrollparallax", (document.body.scrollTop || document.documentElement.scrollTop) * speed);
-    window.requestAnimationFrame( setScrollParallax );
+function setScrollParallax () {
+  title.style.setProperty('--scrollparallax', (document.body.scrollTop || document.documentElement.scrollTop) * speed)
+  window.requestAnimationFrame(setScrollParallax)
 }
 
-window.requestAnimationFrame( setScrollParallax );
+window.requestAnimationFrame(setScrollParallax)
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,29 @@
-var gulp = require('gulp');
-var autoprefix = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-var cp = require('child_process');
-var htmlmin = require('gulp-htmlmin');
-var path = require('path');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const autoprefix = require('gulp-autoprefixer')
+const browserSync = require('browser-sync')
+const cp = require('child_process')
+const htmlmin = require('gulp-htmlmin')
+const path = require('path')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
 
-var deploy = '_site/';
+const deploy = '_site/'
 
-var paths = {
+const paths = {
   scripts: 'assets/js/**',
   images: 'assets/img/**',
   styles: 'assets/css/**',
   markup: ['*.html', '_layouts/*.html', '_includes/*', '_posts/**', '_data/*']
-};
+}
+
 gulp.task('build', function (done) {
   return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
-    .on('close', done);
-});
+    .on('close', done)
+})
 
-gulp.task('htmlmin', ['build'], function() {
+gulp.task('htmlmin', ['build'], function () {
   return gulp.src([
     path.join(deploy, '*.html'),
     path.join(deploy, '*/*.html'),
@@ -30,22 +32,22 @@ gulp.task('htmlmin', ['build'], function() {
     path.join(deploy, '*/*/*/*/*.html'),
     path.join(deploy, '*/*/*/*/*/*.html')
   ])
-  .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
-  .pipe(gulp.dest(deploy));
-});
+    .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(gulp.dest(deploy))
+})
 
 gulp.task('rebuild', ['htmlmin', 'js-uglify'], function () {
-    browserSync.reload();
-});
+  browserSync.reload()
+})
 
-gulp.task('browser-sync', ['sass', 'htmlmin', 'js-uglify'], function() {
+gulp.task('browser-sync', ['sass', 'htmlmin', 'js-uglify'], function () {
   browserSync({
     server: {
       baseDir: '_site'
     },
     notify: false
-  });
-});
+  })
+})
 
 gulp.task('sass', function () {
   return gulp.src('assets/css/main.scss')
@@ -56,22 +58,24 @@ gulp.task('sass', function () {
     .pipe(autoprefix(['last 10 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('_site/assets/css'))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest('assets/css'));
-});
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(gulp.dest('assets/css'))
+})
 
-gulp.task('js-uglify', ['build'], function(cb) {
+gulp.task('js-uglify', ['build'], function (cb) {
   return gulp.src('_site/assets/js/*.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
     .pipe(uglify())
-    .pipe(gulp.dest('_site/assets/js/'))
-});
-
+    .pipe(gulp.dest('_site/assets/js'))
+})
 
 gulp.task('watch', function () {
-  gulp.watch([paths.styles, '!assets/css/main.css'], ['sass']);
-  gulp.watch(paths.images, ['rebuild']);
-  gulp.watch(paths.scripts, ['rebuild']);
-  gulp.watch(paths.markup, ['rebuild']);
-});
+  gulp.watch([paths.styles, '!assets/css/main.css'], ['sass'])
+  gulp.watch(paths.images, ['rebuild'])
+  gulp.watch(paths.scripts, ['rebuild'])
+  gulp.watch(paths.markup, ['rebuild'])
+})
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch'])
